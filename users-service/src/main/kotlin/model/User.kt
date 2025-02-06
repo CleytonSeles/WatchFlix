bee978c3abf350cf1d834a46dcb5fcdc1f769058
cleytonseles <cleyttonselestj@gmail.com>
2025-02-06 10:41:01 -0300

    Criação do endpoint básico '/hello' no UserController

---
+++ users-service/src/main/kotlin/model/User.kt
@@ -0,0 +1,16 @@
+package model
+
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType.IDENTITY
+
+@Entity
+data class User(
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    val id: Long = 0,
+    val username: String,
+    val password: String,
+    val email: String
+)
